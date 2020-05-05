f3697f502254503eb81f996552894ae2203d0635
Nery Chucuy <nerychucuy@gmail.com>
2020-05-04 22:31:30 -0600

    Updates

---
+++ precache-manifest.f6bf5b45ea110701f04ace6ba47bed63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5509c8f49ac9f23c0cc9562ade3e83",
+    "url": "/react-gif-browser/index.html"
+  },
+  {
+    "revision": "81ed732840793ca5f857",
+    "url": "/react-gif-browser/static/css/main.15d9e56a.chunk.css"
+  },
+  {
+    "revision": "a58e025a5dac1267606d",
+    "url": "/react-gif-browser/static/js/2.050858fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gif-browser/static/js/2.050858fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81ed732840793ca5f857",
+    "url": "/react-gif-browser/static/js/main.e0ed2a7d.chunk.js"
+  },
+  {
+    "revision": "d37ac9cdb238d0c54dfb",
+    "url": "/react-gif-browser/static/js/runtime-main.90ba8d73.js"
+  }
+]);
